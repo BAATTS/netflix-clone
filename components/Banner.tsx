89a6f4a8842521de57a00cfa696bc612ad450329
Jorge Zepeda <baattts@gmail.com>
2022-11-20 10:25:28 -0800

    Fixed a typo causing error, and replaced deprecated Image properties

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -6,15 +6,17 @@ import { FaPlay } from "react-icons/fa"
 import { InformationCircleIcon } from "@heroicons/react/outline"
 
 interface Props {
-  netflixOriginals: Movie
+  netflixOriginals: Movie[]
 }
 
 function Banner({netflixOriginals}: Props) {
   const [movie, setMovie] = useState<Movie | null>(null)
+  const lengthOfOriginals = netflixOriginals.length
+  const randomMovie = Math.floor(Math.random() * lengthOfOriginals)
 
   useEffect(() => {
     setMovie(
-      netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)]
+      netflixOriginals[randomMovie]
     )
   }, [netflixOriginals])
 
@@ -24,9 +26,9 @@ function Banner({netflixOriginals}: Props) {
       <div className="absolute top-0 left-0 -z-10 h-[95vh] w-screen">
         <Image 
           src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
-          layout="fill"
-          objectFit="cover"
+          fill
           alt="ImgBanner"
+          className="object-cover"
         />
       </div>
 
